b6a6ff93de68971240b790098dbd82e6a3494469
RoyR2D2 <roybustillobolea@gmail.com>
2024-11-28 01:25:58 +0100

    Version 28/11/2024

--- Java-main-main/Practica4/java/Ejercicio12Clase.js
+++ Java-main-main/Practica4/java/Ejercicio12Clase.js
@@ -1,9 +1,6 @@
-var numero
-var i
-var producto
-i = prompt("introduzca un numero y se le mostrara su tabla de multiplicar")
-for(i = prompt;i <=10;multi ++) {
-    producto = numero * multi
-    
+var numero = prompt("introduzca un numero y se le mostrara su tabla de multiplicar");
+var multi = 1;
+var producto;
+for(i = numero ;multi <=10;multi ++) {
+    document.writeln(i + " x " + multi + " = " + (i * multi) + "<br>");
 }
-document.writeln(i * multi == producto)
--- Java-main-main/Practica4/java/EjercicioAdivina.js
+++ Java-main-main/Practica4/java/EjercicioAdivina.js
@@ -1,7 +1,8 @@
 var numeroaAdivinar = 6;
 var intentosMaximos = 0;
-var numero = prompt("Introzuca un numero entre el uno al diez para adivinar el numero");
-while(numero != numeroaAdivinar && intentosMaximos == 3) {
+var intentos = 0
+var numero = prompt("Introduce un numero entre el uno al diez para adivinar el numero");
+while(numero != numeroaAdivinar && intentosMaximos < 3) {
     alert("No es correcto intentelo de nuevo");
     if (numero < numeroaAdivinar) {
         alert("El numero que buscas es mayor");
@@ -9,10 +10,11 @@ while(numero != numeroaAdivinar && intentosMaximos == 3) {
         alert("El numero que buscas es menor");
     }
     intentosMaximos++;
+    intentos++;
     numero = prompt("Introzuca un numero entre el uno al diez para adivinar el numero");
 }
-if(numero = numeroaAdivinar){
-    document.writeln("Has acertado");
+if(numero == numeroaAdivinar){
+    document.writeln("Has acertado en =" + intentos );
 } else{
     document.writeln("game over");
 }
