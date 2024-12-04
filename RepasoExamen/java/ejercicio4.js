af4d73d2946e9876b6f2a6733baff07722e3046e
RoyR2D2 <roybustillobolea@gmail.com>
2024-12-04 12:00:36 +0100

    Version 04/12/2024

--- RepasoExamen/java/ejercicio4.js
+++ RepasoExamen/java/ejercicio4.js
@@ -1,8 +1,6 @@
 function calcular() {
-    var precio = document.getElementById(precio).value;
-    var iva = document.getElementById(iva).value;
-    precio = parseInt(precio);
-    iva = parseInt(iva);
-    var total = + (precio * iva / 100);
-    document.getElementById("resultado").innerHTML = "El precio con IVA es " + total 
+    var precio = document.getElementById("precio").value;
+    var iva = document.getElementById("iva").value;
+    total = precio + (precio * iva / 100);
+    document.getElementById("resultado").innerHTML = "El precio con IVA es: " + (total);
 }
